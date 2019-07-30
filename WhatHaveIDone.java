261b3a3cf1168378812807d598e3ad8753e851d1
DrMeepster <19316085+DrMeepster@users.noreply.github.com>
2019-07-30 14:59:26 -0700

    Create WhatHaveIDone.java

---
+++ WhatHaveIDone.java
@@ -0,0 +1,14 @@
+import net.minecraft.util.ResourceLocation;
+
+@SuppressWarnings("all")
+public class WhatHaveIDone{
+	public static void main(String[] args) throws Throwable{
+		System.out.println(Palindrome.palindrome("php"));
+		
+		System.out.println(Palindrome.palindrome("Murder for a jar of red rum"));
+		
+		System.out.println(Palindrome.palindrome("123321"));
+		
+		System.out.println(Palindrome.palindrome("Potato"));
+	}
+}
